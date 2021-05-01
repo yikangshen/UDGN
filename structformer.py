e0e038ae5f4b8565c9f4e28c4928615a12d5c91e
YikangShen <yikang.shn@gmail.com>
2021-05-01 16:07:39 -0400

    debug

--- structformer.py
+++ structformer.py
@@ -247,7 +247,8 @@ class StructFormer(nn.Module):
 
         if self.detach_parser:
             att_mask = att_mask.detach()
-            rels = rels.detach()
+            if rels is not None:
+                rels = rels.detach()
 
         return att_mask, head, rels
 
